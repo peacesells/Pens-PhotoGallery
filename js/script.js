e87e73801815e1970105d4e81fbc33f784a3d069
August <nirvanaa@protonmail.ch>
2020-04-09 22:22:01 +0900

    Стилизировал конпки.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let btn_next = document.querySelector('#gallery .buttons .next');
 let images = document.querySelectorAll('#gallery .photos img');
 let i = 0;
 
+const buttons = document.querySelectorAll('a');
+
 btn_prev.onclick = function() {
 	images[i].className = '';
 	i--;
@@ -25,3 +27,20 @@ btn_next.onclick = function() {
 
 	images[i].className = 'showed';
 }
+
+buttons.forEach(btn => {
+	btn.addEventListener('click', function(e) {
+
+		let x = e.clientX - e.target.offsetLeft;
+		let y = e.clientY - e.target.offsetTop;
+
+		let ripples = document.createElement('span');
+		ripples.style.left = x + 'px';
+		ripples.style.top = y + 'px';
+		this.appendChild(ripples);
+
+		setTimeout(() => {
+			ripples.remove()
+		}, 1000);
+	})
+})
